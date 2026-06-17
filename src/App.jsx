c73d380126a7a844b45a877063c52bbc7bbf2002
Ansh Raj Kumar <golu.kkpncc.99008@gmail.com>
2026-06-17 20:04:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './componentsJsx/Navbar'
+
+const App = () => {
+    return (
+        <main>
+            <Navbar />
+        </main>
+    )
+}
+
+export default App
