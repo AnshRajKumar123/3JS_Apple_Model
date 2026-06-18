4e6c017c6e0dd63a1bf896344cab1a9ee9fad500
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-06-18 22:27:06 +0530

    Model Changer commit

--- src/componentsJsx/three/ModelSwitcher.jsx
+++ src/componentsJsx/three/ModelSwitcher.jsx
@@ -3,6 +3,28 @@ import React, { useRef } from 'react'
 
 import MacbookModel16 from '../models/Macbook-16';
 import MacbookModel14 from '../models/Macbook-14';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+const ANIMATION_DURATION = 1;
+const OFFSET_DISTANCE = 5;
+
+const fadeMeshes = (group, opacity) => {
+    if (!group) return;
+
+    group.traverse((child) => {
+        if (child.isMesh) {
+            child.material.transparent = true;
+            gsap.to(child.material, { opacity, duration: ANIMATION_DURATION })
+        }
+    })
+}
+
+const moveGroup = (group, x) => {
+    if (!group) return;
+
+    gsap.to(group.position, { x, duration: ANIMATION_DURATION })
+}
 
 const ModelSwitcher = ({ scale, isMobile }) => {
 
@@ -11,13 +33,31 @@ const ModelSwitcher = ({ scale, isMobile }) => {
 
     const showLargeMacbook = scale === 0.08 || scale === 0.05;
 
+    useGSAP(() => {
+
+        if (showLargeMacbook) {
+            moveGroup(smallMacbookRef.current, -OFFSET_DISTANCE);
+            moveGroup(largeMacbookRef.current, 0);
+
+            fadeMeshes(smallMacbookRef.current, 0);
+            fadeMeshes(largeMacbookRef.current, 1);
+        } else {
+            moveGroup(smallMacbookRef.current, 0);
+            moveGroup(largeMacbookRef.current, -OFFSET_DISTANCE);
+
+            fadeMeshes(smallMacbookRef.current, 1);
+            fadeMeshes(largeMacbookRef.current, 0);
+        }
+
+    }, [scale])
+
     const controlsConfig = {
         snap: true,
         speed: 1,
         zoom: 1,
         polar: [-Math.PI, Math.PI],
         azimuth: [-Infinity, Infinity],
-        // config: { mass: 1, tension: 0, friction: 26 },
+        config: { mass: 1, tension: 0, friction: 26 },
     }
 
     return (
@@ -28,11 +68,11 @@ const ModelSwitcher = ({ scale, isMobile }) => {
                 </group>
             </PresentationControls>
 
-            {/* <PresentationControls {...constrolsConfig}>
+            <PresentationControls {...controlsConfig}>
                 <group ref={smallMacbookRef}>
                     <MacbookModel14 scale={isMobile ? 0.03 : 0.06} />
                 </group>
-            </PresentationControls> */}
+            </PresentationControls>
         </>
     )
 }
