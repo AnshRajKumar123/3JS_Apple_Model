8359cd167d0292d5ea62701fcb32fbaca9612034
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-06-18 14:52:27 +0530

    Add Lightting and Model

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './componentsJsx/Navbar'
 import Hero from './componentsJsx/Hero'
+import ProductViwer from './componentsJsx/ProductViwer'
 
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero />
+            <ProductViwer />
         </main>
     )
 }
