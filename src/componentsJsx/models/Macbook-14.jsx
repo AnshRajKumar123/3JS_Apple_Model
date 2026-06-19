0d6d68262268c14fffc8ca260f4efa76e0ac762a
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-06-19 08:56:08 +0530

    Some Changes

--- src/componentsJsx/models/Macbook-14.jsx
+++ src/componentsJsx/models/Macbook-14.jsx
@@ -13,7 +13,7 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import { texture3D } from 'three/tsl';
 import useMacbookStore from '../../store/index';
 import { noChangeParts } from '../../constants/index';
-import { Color } from 'three'
+import { Color, SRGBColorSpace } from 'three'
 
 export default function MacbookModel14(props) {
 
@@ -21,6 +21,8 @@ export default function MacbookModel14(props) {
   const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb')
 
   const texture = useTexture('/screen.png');
+  texture.colorSpace = SRGBColorSpace;
+  texture.needsUpdate = true;
 
   useEffect(() => {
     scene.traverse((child) => {
